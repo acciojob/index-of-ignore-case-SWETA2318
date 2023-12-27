9f29fff59074e308b1643edf36deff5446646260
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-27 12:14:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Handle edge cases
+  if (s1 === "" || s2 === "") {
+    return -1;
+  }
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerS1 = s1.toLowerCase();
+  const lowerS2 = s2.toLowerCase();
+
+  // Use indexOf for case-insensitive search
+  const index = lowerS1.indexOf(lowerS2);
+
+  return index;
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
+
 alert(indexOfIgnoreCase(s1, s2));
